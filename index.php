5db9f7002284fabecb5b3960e9d0185a0f17017a
Cliente <Cliente@CLIENTE-PC>
2020-04-03 03:37:53 -0300

    lucas 20

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Investimentos</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+ 
+</head>
+<body>    
+    
+
+    <div class="container mt-4">
+        <div class="card bg-success">
+            <div class="card text-center">
+            <h2>Simular Invetimentos</h2>
+                </div>
+
+                <form  method="GET" action="envio.php" >
+
+                        <div class="card bg-success">
+                            <div class="row">
+                                <div class="col-md-2"></div>
+                                <div class="col-md-8">
+                                    <h5>Valor Inicial:</h5>
+                                    <input type="text" class="form-control" name="valor" required><br>
+                                    
+                                </div>
+                                <div class="col-md-2"></div>
+                            </div>
+                        </div>
+
+                        <div class="card-footer">
+                           <div class="row">
+                              <div class="col-md-2"></div>
+                                <div class="col-md-8">
+                                  <button type="submit" class="btn btn-light btn-block">Enviar</button>   
+                                </div>             
+                              <div class="col-md-2"></div>
+                            </div>
+                        </div>
+
+                <form>
+
+            </div>
+
+
+    </div> 
+
+    <div><br></div>
+
+    <script src="jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    
+</body>
+</html>
